8d80a35326bf7100544c71843737b950183ce701
TuckerBane <Tucker.bane@icloud.com>
2016-09-29 22:57:58 -0700

    Crafting work started

---
+++ SuperAIDemo/Assets/Scripts/Crafting/CraftingSystem.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class CraftingRecipe
+{
+    public GameObject m_createdObjectPrefab;
+    public List<GameObject> m_craftingComponents;
+    // optional recipe components
+    public float m_time = 0.0f;
+    public GameObject[] m_required_tools;
+}
+
+public class CraftingSystem : MonoBehaviour {
+
+    public GameObject[] m_baseMaterials;
+    public CraftingRecipe[] m_recipes;
+    public CraftingRecipe m_testing;
+    public int i;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
