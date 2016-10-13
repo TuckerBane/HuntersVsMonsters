8bea87d961ed1803bf79d3553ba4284f3f2b7693
TuckerBane <Tucker.bane@icloud.com>
2016-10-12 22:34:12 -0700

    ~2 hours Nodes in
    planning now goes in nodes

--- SuperAIDemo/Assets/Scripts/AI/PlaningAI.cs
+++ SuperAIDemo/Assets/Scripts/AI/PlaningAI.cs
@@ -2,18 +2,40 @@
 using System.Collections;
 using System.Collections.Generic;
 
-public struct RecipeNode
+[System.Serializable]
+public class RecipeNode
 {
     public CraftingRecipe m_recipe;
     // lack of pointers/references makes me sad
-    // public List<RecipeNode > m_requiredRecipes;
+    public List<int> m_requiredRecipeIndexes = new List<int>();
 }
 
 public class PlaningAI : MonoBehaviour {
 
+    public List<RecipeNode> m_recipeNodes = new List<RecipeNode>();
+    public List<int> m_reusableIndexes = new List<int>();
+    public RecipeNode GetNewRecipeNode(ref int index)
+    {
+        if (m_reusableIndexes.Count == 0)
+        {
+            m_recipeNodes.Add(new RecipeNode());
+            index = m_recipeNodes.Count - 1;
+            return m_recipeNodes[m_recipeNodes.Count - 1];
+        }
+        else
+        {
+            int i = m_reusableIndexes.Count - 1;
+            RecipeNode node = m_recipeNodes[m_reusableIndexes[i]];
+            index = m_reusableIndexes[i];
+            m_reusableIndexes.RemoveAt(i);
+            return node;
+        }
+    }
+
+
     public GameObject m_objectivePrefab;
+    public RecipeNode m_goalRootNode;
     private CraftingSystem m_craftingSystem;
-    private CraftingRecipe m_goalRecipe;
     private ActionList m_myActions;
     
 
@@ -26,8 +48,10 @@ public class PlaningAI : MonoBehaviour {
 
     void MakePlan()
     {
-        m_goalRecipe = m_craftingSystem.GetRecipe(m_objectivePrefab);
+        RecipeNode m_goalRecipe = new RecipeNode();
+        m_goalRecipe.m_recipe = m_craftingSystem.GetRecipe(m_objectivePrefab);
         ExtendRecipeChain(m_goalRecipe);
+        m_goalRootNode = m_goalRecipe;
 
         // HACK copy paste
         GoSomewhere go = new GoSomewhere();
@@ -35,7 +59,7 @@ public class PlaningAI : MonoBehaviour {
         m_myActions.m_list.Add(go); // go to crafting table
 
         CraftSomething craft = new CraftSomething();
-        craft.m_recipe = m_goalRecipe;
+        craft.m_recipe = m_goalRecipe.m_recipe;
         craft.m_craftingSystem = m_craftingSystem;
         m_myActions.m_list.Add(craft); // craft the thing
 
@@ -46,14 +70,20 @@ public class PlaningAI : MonoBehaviour {
         m_myActions.m_list.Add(new Drop()); // drop what you made
     }
 	
-    void ExtendRecipeChain(CraftingRecipe recipe)
+    void ExtendRecipeChain(RecipeNode recipe)
     {
-        foreach(GameObject component in recipe.m_craftingComponents)
-        {
+        foreach(GameObject component in recipe.m_recipe.m_craftingComponents)
+        { 
             CraftingRecipe newRecipe = m_craftingSystem.GetRecipe(component);
+
             if (newRecipe != null)
             {
-                ExtendRecipeChain(newRecipe);
+                int recipeIndex = 0;
+                RecipeNode newNode = GetNewRecipeNode(ref recipeIndex);
+                newNode.m_recipe = newRecipe;
+                recipe.m_requiredRecipeIndexes.Add(recipeIndex);
+
+                ExtendRecipeChain( newNode );
 
                 // add use recipe to action list
                 GoSomewhere go = new GoSomewhere();
